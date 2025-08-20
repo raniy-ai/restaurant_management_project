c350e894a3567829e118d6a773c64d2ee47d42b4
raniy-ai <raniy5181@gmail.com>
2025-08-20 22:04:59 +0530

    33771

--- home/views.py
+++ home/views.py
@@ -6,3 +6,13 @@ def home(request):
     }
     return render(request, 'home.html',context)
 # Create your views here.
+
+def menu_view(request):
+
+    menu_items=[
+        {"name":"Margherita Pizza", "price":250},
+        {"name":"Pasta Alfredo","price":300 },
+        {"name":"Veg Burger","price":150},
+        {"name":"French Fires","prices"100},
+    ]
+    return render(request, "menu.html",{"menu_items":menu_items})
