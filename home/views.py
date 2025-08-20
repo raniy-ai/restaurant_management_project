14590932ad4ac39510bcff14672c4ceb6cc30c49
raniy-ai <raniy5181@gmail.com>
2025-08-20 21:35:06 +0530

    33774

--- home/views.py
+++ home/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'home.html',{'restuarant_name':'Tasty Bites'})
+    'restuarant_phone':settings.RESTUARANT_PHONE,
+    }
+    return render(request, 'home.html',context)
 # Create your views here.
